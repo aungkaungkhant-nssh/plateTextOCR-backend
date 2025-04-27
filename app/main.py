d9d21af32333394a614fe4b54e4fb676d24e3cbd
aungkaungkhant-nssh <akkgit0909@gmail.com>
2025-04-27 10:21:22 +0630

    remove face_recognition

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, File,UploadFile
 from fastapi.responses import JSONResponse
 import cv2
 from fastapi.middleware.cors import CORSMiddleware
-import face_recognition
 from app.routes import vehicle
 from app.database import engine, Base
 
@@ -23,22 +22,22 @@ app.include_router(vehicle.router)
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
-@app.post("/check-person")
-async def check_person(file: UploadFile=File(...)):
-    img =cv2.imread("./data/akk.png")
-    rgb_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    img_encoding= face_recognition.face_encodings(rgb_img)[0]
+# @app.post("/check-person")
+# async def check_person(file: UploadFile=File(...)):
+#     img =cv2.imread("./data/akk.png")
+#     rgb_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#     img_encoding= face_recognition.face_encodings(rgb_img)[0]
 
-    img2 =cv2.imread("./data/messi.jpeg")
-    rgb_img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
-    img_encoding2= face_recognition.face_encodings(rgb_img2)[0]
+#     img2 =cv2.imread("./data/messi.jpeg")
+#     rgb_img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
+#     img_encoding2= face_recognition.face_encodings(rgb_img2)[0]
 
-    print(img_encoding)
-    print("---------")
-    print(img_encoding2)
+#     print(img_encoding)
+#     print("---------")
+#     print(img_encoding2)
 
-    result =face_recognition.compare_faces([img_encoding],img_encoding2)
-    print(result)
+#     result =face_recognition.compare_faces([img_encoding],img_encoding2)
+#     print(result)
     
 
 
--- render-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y cmake g++ build-essential libboost-all-dev libopenblas-dev liblapack-dev
-pip install -r requirements.txt

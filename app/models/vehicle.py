dd6f17593dd1a5fa92cc96d9272dee33e5424e22
aungkaungkhant-nssh <akkgit0909@gmail.com>
2025-05-01 10:38:20 +0630

    alembic for migration

--- app/models/vehicle.py
+++ app/models/vehicle.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,DateTime
 from app.database import Base
+from sqlalchemy.sql import func
 
 class Vehicle(Base):
     __tablename__ = "vehicles"
 
     id = Column(Integer, primary_key=True, index=True)
     plate_number = Column(String, unique=True)
+    punishment_count = Column(Integer, default=0)
+    punishment_amount = Column(Integer, default=30000)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
